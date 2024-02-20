dab6a6ed8240d7f2d6a9b2055e9fc5dc47c5d70d
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-20 21:30:52 +0530

    Add files via upload

---
+++ HTML & CSS/Basics/practice/again.js
@@ -0,0 +1,10 @@
+let uname=document.getElementById('username')
+let pass  = document.getElementById('password')
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click',()=>{
+    let x = uname.value
+    let y = pass.value
+    
+    console.log('clicked')
+})
---
+++ HTML & CSS/Basics/practice/prac.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('btn')
+let userNname = document.getElementById('uname')
+
+btn.addEventListener('click',()=>{
+    let x = userNname.value
+    console.log(x)
+    console.log('clicked')
+})
