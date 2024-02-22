c9401025dad9307f8bda2516b8d643336df81edb
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-22 15:07:47 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Pass by value, ref.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// ----------------------------PASS BY VALUE------------------
+
+// int square(int a){
+//     return a*a;
+// }
+
+// int main(){
+//     int a=5;
+//     cout<<square(a)<<endl<<"a : "<<a;
+// }
+
+
+// ----------------------------PASS BY REF---------------------
+
+// void square(int &b){
+//     b= b*b;
+// }
+
+// int main(){
+//     int b=5;
+//     square(b);
+//     cout<<"b : "<<b;
+// }
