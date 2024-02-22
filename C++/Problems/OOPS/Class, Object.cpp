b6022202f2672df2a749d59e322a7bcafd40e0f7
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-22 14:57:37 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Class, Object.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Room{
+    int length;
+    int breadth;
+    public:
+        void getResult(int l,int b){
+            length =l;
+            breadth = b;
+        }
+        int calculateArea(){
+            return length*breadth;
+        }
+};
+int main(){
+    Room r1;
+    r1.getResult(50,100);
+    cout<<r1.calculateArea();
+}
