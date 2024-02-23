4616787aff9dafcba2d9ebb360e66060546540ec
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-23 09:37:36 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Memory Allocation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *p = new int(10);
+    cout<<*p<<endl;
+    
+    int *p2 = new int[20];
+    
+    delete p;
+    
+    delete []p2;
+    
+    cout<<p2;
+}
