04804598f9a5260f222104d8016d269e3113e19b
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-23 09:28:45 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Default Arguments.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int product(int a, int b,int c=1,int d=1){
+    return a*b*c*d;
+}
+
+int main(){
+    cout<<product(5,6);
+}
+
+// it should be done only from the last and no middle assignment should be done..
