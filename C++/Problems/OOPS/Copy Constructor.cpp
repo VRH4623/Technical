be72695611ea0a634070e9daa7031148a60664c6
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-23 09:38:24 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Copy Constructor.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Final{
+    int real;
+    int imag;
+    public:
+        Final(int r, int i){
+            real = r;
+            imag = i;
+        }
+        Final(const Final &F){
+            real = F.real;
+            imag = F.imag;
+        }
+        void display(){
+            cout<<real<<"+"<<imag<<"i";
+        }
+};
+
+int main(){
+    Final f1(5,10);
+    Final f2 = f1;
+    f2.display();
+}
+// copy constructor - > getting object as input and copying it to another constructor
