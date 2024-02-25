a82e780de6e29b655f13e67c25e97cf5ec86e8ae
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-25 11:25:18 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Protected.cpp
@@ -0,0 +1,59 @@
+    #include<bits/stdc++.h>
+using namespace std;
+
+class Employee{
+    string name;
+    double salary;
+    protected:
+    int empId;
+    public:
+        Employee(int eId, string n, double sal=0){
+            empId = eId;
+            name = n;
+            salary = sal;
+        }
+        void print(){
+            cout<<empId<<endl<<name<<endl<<salary<<endl;
+        }
+        void setName(string n){
+            name =n;
+        }
+        string getName(){
+            return name;
+        }
+        void setSalary(double sal){
+            salary = sal;
+        }
+        double getSalary(){
+            return salary;
+        }
+};
+
+class ContractEmployee : public Employee{
+    int workHours;
+    public:
+        ContractEmployee(int eId, string n, int wH) :Employee(eId,n){
+            // calling base class constructor
+            workHours = wH;
+            empId = eId;
+            setSalary();
+        }
+        void setSalary(){
+            Employee::setSalary(workHours*100);
+        }
+        void print(){
+            Employee::print();
+            cout<<workHours<<endl;
+        }
+};
+
+int main(){
+    Employee e1(1,"RAJ",25000);
+    e1.print();
+    
+    ContractEmployee ce2(2,"Hemanth",290);
+    ce2.print();
+}
+
+
+
