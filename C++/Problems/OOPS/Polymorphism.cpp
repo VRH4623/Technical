81d3835bcfc6b608c16da74c365785089daf00aa
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-25 11:26:25 +0530

    Add files via upload

---
+++ C++/Problems/OOPS/Polymorphism.cpp
@@ -0,0 +1,14 @@
+//                              Polymorphism
+//                                  |
+//                  ________________|______________________________
+//                 |                                             |
+//             Compile Time                                    Run-Time
+//                 |                                             |
+//       __________________________                          Virtual Functions
+//      |                        |                      
+//  Function Ovldng       Operator Ovldng   
+
+
+// compile time -> argumentss will be known during the compiling time itself.
+// vitual function -> function call haopens acccording to the pointer pointing the object.
+// polymorphism -> function overlaoding (or) adopting behaviour according to the scenario
