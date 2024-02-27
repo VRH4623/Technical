13c0a038a840669568a102ea6282c0d2433fd364
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-27 11:01:36 +0530

    Add files via upload

---
+++ C++/Problems/Other/Number to roman.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 3549;
+    
+    int a[] ={1,4,5,9,10,40,50,90,100,400,500,900,1000 };
+    string s[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};  
+    int i= 12;
+    
+    while(n>0){
+        int quo = n/a[i];
+        n = n%a[i];
+        
+        while(quo--){
+            cout<<s[i];
+        }
+        i--;
+    }
+}
