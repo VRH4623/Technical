3ec8a7c4a6cff8d6611b994af9ba1c550221593b
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-02-29 08:55:01 +0530

    Update main.js

--- Javascript/Learning/learnt1/main.js
+++ Javascript/Learning/learnt1/main.js
@@ -1,6 +1,6 @@
 const myForm = document.querySelector('#my-form')
 const nameInput = document.querySelector('#name')
-const emailInput = document.querySelector('#email')
+const emailInput = document.querySelector('#email') 
 const msg = document.querySelector('.msg')
 const userList = document.querySelector('#users')
  
