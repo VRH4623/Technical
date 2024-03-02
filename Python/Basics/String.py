6898286cdf607645ee8d220357b5e8ee3052d46c
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-03-02 18:04:00 +0530

    Add files via upload

---
+++ Python/Basics/String.py
@@ -0,0 +1,16 @@
+'''
+para = "hey hemanth how are you"
+final = para[2:-2]
+
+print(final)
+'''
+
+'''
+first = 'Hemanth'
+second = 'vrh4623'
+message = f'{first} [{second}] is a coder'
+print(message)
+'''
+
+course = 'Python, for beginners'
+print(course.zfill(5))
