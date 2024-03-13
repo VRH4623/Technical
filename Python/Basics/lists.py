39b4843d80494bd51680875f0be8efc1b0c3e14b
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-03-13 10:34:18 +0530

    Update lists.py

--- Python/Basics/lists.py
+++ Python/Basics/lists.py
@@ -1,7 +1,22 @@
-numbers = [1,3,5,2,89,8,5,12]
-maxElem = 0
-for itr in numbers:
-    if(itr>maxElem):
-        maxElem=itr
+numbers = [1, 3,5,2,89,8,5,12]
+print(len(numbers))
+numbers.insert(1, 100)
+print(numbers)
+print(len(numbers))
 
-print(maxElem)
+numbers.remove(89)
+print(numbers)
+
+numbers.pop()
+print(numbers)
+
+print(8 in numbers)
+numbers.sort()
+numbers.reverse()
+print(numbers)
+print(numbers)
+
+numbers2 = numbers.copy()
+print(numbers2)
+
+
