9f28f5ce5ed29a75e75b7e1332b324bc67157cd1
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-03-13 11:14:23 +0530

    Add files via upload

---
+++ Python/Basics/unpacking.py
@@ -0,0 +1,14 @@
+'''
+x = cordinates[0]
+y = cordinates[1]
+z = cordinates[2]
+
+achieveing this below through unpacking
+'''
+cordinates = (1,2,3)
+x,y,z = cordinates
+print(x,y,z)
+
+list = [10,20,30]
+a,b,c = list
+print(a,b,c)
