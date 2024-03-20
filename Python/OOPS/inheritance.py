0fa63c0eba83d819469669a2f620dad5981ba945
Hemanth <107045069+VRH4623@users.noreply.github.com>
2024-03-20 11:05:31 +0530

    Add files via upload

---
+++ Python/OOPS/inheritance.py
@@ -0,0 +1,14 @@
+class Mammal:
+    def walk(self):
+        print("Walk")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("Bark")
+
+
+class Cat(Mammal):
+    def walk(self):
+        print("Walk")
+
